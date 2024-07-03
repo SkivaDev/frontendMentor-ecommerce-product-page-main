65d61e4a3efd1bd31f7a6e4c943c7ffb806daf79
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-07-03 08:40:21 -0500

    style(project): :lipstick: update tailwind styles config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       backgroundColor: {
-        orange: "hsl(26, 100%, 55%)",
+        "orange": "hsl(26, 100%, 55%)",
         "pale-orange": "hsl(25, 100%, 94%)",
         "very-dark-blue": "hsl(220, 13%, 13%)",
         "dark-grayish-blue": "hsl(219, 9%, 45%)",
@@ -12,7 +12,7 @@ export default {
         "light-grayish-blue": "hsl(223, 64%, 98%)",
       },
       colors: {
-        orange: "hsl(26, 100%, 55%)",
+        "orange": "hsl(26, 100%, 55%)",
         "pale-orange": "hsl(25, 100%, 94%)",
         "very-dark-blue": "hsl(220, 13%, 13%)",
         "dark-grayish-blue": "hsl(219, 9%, 45%)",
