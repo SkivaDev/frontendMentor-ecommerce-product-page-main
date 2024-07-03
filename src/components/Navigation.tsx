bbfc6ab6318daa9bcc0dde1ad68a9452a081443c
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-07-03 15:36:39 -0500

    style(Navigation): :art: update styles for desktop design

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,7 @@ const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="relative flex justify-between items-center gap-[57px] mt-[33px] md:mt-[33px] px-[22.6px] md:px-[39.6px] w-full max-w-[1190px] mx-auto">
+    <nav className="relative flex justify-between items-center gap-[57px] my-[33px] md:mt-[28px] px-[22.6px] md:px-[39.6px] w-full max-w-[1190px] mx-auto">
       <a href="/">
         <img className="max-w-max" src="./images/logo.svg" alt="shortly logo" />
       </a>
@@ -13,7 +13,7 @@ const Navigation = () => {
           isOpen ? "translate-y-[20%]" : "translate-y-[-120%]"
         } flex md:flex flex-col md:flex-row justify-between items-center absolute md:relative gap-8 md:gap-0 p-8 md:p-0 top-1/2 md:top-0 left-1/2 md:left-0 translate-x-[-50%]  text-white md:text-black md:transform-none bg-dark-violet md:bg-transparent w-full max-w-[90%] md:max-w-none rounded-lg md:rounded-none transition-all duration-500`}
       >
-        <ul className="flex flex-col md:flex-row gap-[31.12px] md:mr-[46.4px] text-[20px] md:text-[15px] font-bold text-center md:text-left text-grayish-violet">
+        <ul className="flex flex-col md:flex-row gap-[35px] md:mr-[46.4px] text-[20px] md:text-[15px] font-medium text-center md:text-left text-grayish-violet">
           <li>
             <a href="/" className="hover:text-black transition-all">
               Collections
@@ -40,7 +40,7 @@ const Navigation = () => {
             </a>
           </li>
         </ul>
-        <div className="flex flex-col md:flex-row justify-center items-center gap-[34px] text-[20px] md:text-[16px] w-full md:w-auto pt-[30px] md:pt-0 border-t border-grayish-violet md:border-none">
+        <div className="flex flex-col md:flex-row justify-center items-center gap-[46px] text-[20px] md:text-[16px] w-full md:w-auto pt-[30px] md:pt-0 border-t border-grayish-violet md:border-none">
           {/* <a href="" className="font-bold text-grayish-violet hover:text-black">
             Login
           </a> */}
