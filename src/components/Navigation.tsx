52d418b7e89b8fc4f53a2254949835c8acdbf931
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-07-02 19:43:48 -0500

    feat(Navigation): :art: add content nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
