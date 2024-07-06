8c8ef286b56aab8a9965670eeb17b0212a76574c
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-07-05 21:21:04 -0500

    style(Navigation): :lipstick: update with of buttons styles for desktop design

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -19,13 +19,13 @@ const Navigation = () => {
       <a href="/" className="ml-[32px] md:m-0">
         <img className="max-w-max" src="./images/logo.svg" alt="shortly logo" />
       </a>
-      <div className="flex md:flex flex-col md:flex-row justify-between items-center w-full">
+      <div className="flex md:flex flex-col md:flex-row justify-between items-center w-full h-full">
         <div
           className={`${
             isOpen ? "translate-y-0" : "translate-y-[-120%]"
           }  absolute md:relative gap-8 md:gap-0 p-8 py-[70px] md:p-0 top-[-17px] md:top-0 right-[-47px] md:right-0 translate-x-[-50%]  text-black md:text-black md:transform-none w-full max-w-[75vw] md:max-w-none transition-all duration-500 bg-white h-screen md:h-auto`}
         >
-          <ul className="flex flex-col md:flex-row gap-[35px] md:mr-[46.4px] text-[20px] md:text-[15px] font-medium text-grayish-violet">
+          <ul className="flex flex-col md:flex-row gap-[33px] text-[20px] md:text-[15px] h-full font-medium">
             <li>
               <a href="/" className="hover:text-black transition-all">
                 Collections
@@ -53,7 +53,7 @@ const Navigation = () => {
             </li>
           </ul>
         </div>
-        <div className="flex md:flex-row justify-end items-center gap-[22px] md:gap-[46px] text-[20px] md:text-[16px] w-full md:w-auto md:pt-0 border-t border-grayish-violet md:border-none">
+        <div className="flex justify-end items-center gap-[22px] md:gap-[46px] w-full">
           <button>
             <img src="/images/icon-cart.svg" alt="icon cart" />
           </button>
