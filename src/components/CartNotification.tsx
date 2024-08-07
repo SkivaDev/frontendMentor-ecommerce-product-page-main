4d9c91bbea0f61a8d1681e54859c70acd0abed5a
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-08-07 11:20:09 -0500

    feat(project): :sparkles: add CartNotification component

---
+++ src/components/CartNotification.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const CartNotification = () => {
+  return (
+    <div>
+      <h3>Cart</h3>
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <h4>Product Name</h4>
+          <div>
+            <p>Price</p>
+            <p>Quantity</p>
+          </div>
+        </div>
+        <button>
+            <img src="" alt="" />
+        </button>
+      </div>
+      <button>
+        Checkout
+      </button>
+    </div>
+  );
+};
+
+export default CartNotification;
