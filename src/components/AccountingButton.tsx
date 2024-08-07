eccf8eabed57e3e081011d82d31fb406c577b3de
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-08-07 09:51:07 -0500

    style(AccountingButton): :lipstick: update styles for minus and plus buttons

--- src/components/AccountingButton.tsx
+++ src/components/AccountingButton.tsx
@@ -3,17 +3,17 @@ import React, { useState } from "react";
 const AccountingButton = ({amount, setAmount}:{amount: number; setAmount: React.Dispatch<React.SetStateAction<number>>}) => {
 
   return (
-    <div className="flex justify-between items-center w-[159px] py-[16px] px-[17px] font-bold bg-light-grayish-blue rounded-[10px]">
+    <div className="flex justify-between items-center w-[159px] h-[56px] font-bold bg-light-grayish-blue rounded-[10px]">
       <button
         onClick={() => setAmount((amount) => amount > 0 ? amount - 1 : 0)}
-        className="text-[18px] font-bold text-orange"
+        className="h-full text-[18px] px-[17px] font-bold text-orange"
       >
         <img src="/images/icon-minus.svg" alt="icon-minus" />
       </button>
       {amount}
       <button
         onClick={() => setAmount((amount) => amount + 1)}
-        className="text-[18px] font-bold text-orange"
+        className="h-full text-[18px] px-[17px] font-bold text-orange"
       >
         <img src="/images/icon-plus.svg" alt="icon-plus" />
       </button>
